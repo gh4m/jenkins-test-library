6c68b05904bfad0ff1f39fa84060222c4738271e
devuser <devuser@localhost>
2017-06-26 10:29:43 -0400

    try this

--- src/org/foo/Zot.groovy
+++ src/org/foo/Zot.groovy
@@ -3,9 +3,8 @@ package org.foo;
 
 // test
 class Zot(String filename1) {
-  String filename2 = this filename1;
-  def createFile(filename2) {
-    sh: "echo thisisit > ${filename2}"
+  def createFile(filename1) {
+    sh: "echo thisisit > ${filename1}"
   }
 }
 
