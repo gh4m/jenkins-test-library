7920d7d58e152ca013491a518a430e79fe654ecf
devuser <devuser@localhost>
2017-06-27 17:01:00 -0400

    Revert "save"
    This reverts commit 529a8df29172c285707044f01e2782efd7c3454a.

--- src/org/foo/Pot.groovy
+++ src/org/foo/Pot.groovy
@@ -1,9 +1,7 @@
 package org.foo;
 
-class Pot() {
 def createNewFile(filename1) {
   echo "this is the  file  $filename1 that"
   println "date".execute().text
   "date".execute().text > filename1
-}
 }
